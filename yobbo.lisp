9d7cc369339876921d7a7f3b03cdd64a89e8928b
death <death@nessers.org>
2014-12-29 22:59:32 +0200

    redesigned it a bit

--- yobbo.lisp
+++ yobbo.lisp
@@ -4,153 +4,205 @@
 
 (defpackage #:yobbo
   (:import-from #:alexandria #:removef)
+  (:import-from #:constantia #:speaker #:fire #:define-event
+                #:add-listener #:remove-listener)
   (:use #:cl)
   (:export
    #:job
    #:job-name
    #:job-function
    #:job-schedule
-   #:job-last-run
    #:job-active-p
-   #:job-next-run
+   #:job-started
+   #:job-ended
+   #:job-error
+   #:job-warn
+   #:job-cancelled
+   #:job-done
+   #:object
    #:scheduler
+   #:scheduler-status
+   #:scheduler-log
+   #:format-control
+   #:format-arguments
    #:start
-   #:whine
    #:command))
 
 (in-package #:yobbo)
 
 ;;; Job
 
-(defclass job ()
+(defclass job (speaker)
   ((name :initarg :name :accessor job-name)
    (function :initarg :function :accessor job-function)
    (schedule :initarg :schedule :accessor job-schedule)
-   (last-run :initarg :last-run :accessor job-last-run)
    (active :initarg :active :accessor job-active-p))
-  (:default-initargs :last-run nil :active t))
+  (:default-initargs :active t))
 
 (defmethod print-object ((job job) stream)
   (print-unreadable-object (job stream :type t)
     (format stream "~S" (job-name job)))
   job)
 
-(defmethod job-next-run ((job job))
-  (let ((schedule (job-schedule job))
-        (last-run (job-last-run job))
-        (active (job-active-p job)))
-    (cond ((or (not active)
-               (eq schedule :never))
+(define-event job-started)
+(define-event job-ended)
+(define-event job-error object)
+(define-event job-warn object)
+(define-event job-cancelled object)
+(define-event job-done object)
+
+(define-condition cancel () ())
+
+(defmethod run ((job job))
+  (fire job 'job-started)
+  (handler-case
+      (handler-bind ((warning
+                      (lambda (condition)
+                        (fire job 'job-warn :object condition))))
+        (funcall (job-function job)))
+    (error (condition)
+      (fire job 'job-ended)
+      (fire job 'job-error :object condition))
+    (cancel (condition)
+      (fire job 'job-ended)
+      (fire job 'job-cancelled :object condition))
+    (:no-error (&rest results)
+      (fire job 'job-ended)
+      (fire job 'job-done :object (copy-list results)))))
+
+(defmethod job-next-run ((job job) prior-start-time)
+  (let ((schedule (job-schedule job)))
+    (cond ((or (null schedule) (eq schedule :never)
+               (not (job-active-p job)))
            nil)
-          ((and (numberp schedule)
-                (or (null last-run)
-                    (< last-run schedule)))
-           schedule)
-          ((and (consp schedule)
-                (eq (car schedule) :every))
-           (if (null last-run)
+          ((numberp schedule)
+           (if (and prior-start-time (>= prior-start-time schedule))
+               nil
+               schedule))
+          ((and (consp schedule) (eq (car schedule) :every))
+           (if (null prior-start-time)
                (get-universal-time)
-               (+ last-run (cadr schedule))))
+               (+ prior-start-time (cadr schedule))))
           (t nil))))
 
 ;;; Scheduler
 
-(defclass scheduler ()
+(defclass entry ()
+  ((start-time :initarg :start-time :accessor start-time)
+   (end-time :initarg :end-time :accessor end-time)
+   (result :initarg :result :accessor result)
+   (status :initarg :status :accessor status)
+   (thread :initarg :thread :accessor thread)
+   (listener :initarg :listener :accessor listener)))
+
+(defclass scheduler (speaker)
   ((command-queue :initform (lq:make-queue) :accessor command-queue)
    (jobs :initform '() :accessor jobs)
-   (job-threads :initform (make-hash-table) :accessor job-threads)))
+   (entries :initform (make-hash-table) :accessor entries)
+   (lock :initform (bt:make-lock "Scheduler lock") :accessor lock)))
+
+(define-event scheduler-status)
+(define-event scheduler-log format-control format-arguments)
 
 (defgeneric start (scheduler))
-(defgeneric whine (scheduler control-string &rest args))
 (defgeneric command (scheduler op &rest args))
-(defgeneric job-thread (job scheduler))
-(defgeneric (setf job-thread) (new-value job scheduler))
-(defgeneric run-job (job scheduler))
-(defgeneric cancel-job (job scheduler))
-(defgeneric start-job (job scheduler))
-(defgeneric call-job (job scheduler))
-(defgeneric scan-jobs (scheduler))
+(defgeneric whine (scheduler control-string &rest args))
 (defgeneric scheduler-loop (scheduler))
 (defgeneric process-command (scheduler op &key))
+(defgeneric scan-jobs (scheduler))
+(defgeneric scheduler-job-event (scheduler job event))
 
 (defmethod start ((scheduler scheduler))
   (bt:make-thread (lambda () (scheduler-loop scheduler))
                   :name "Job scheduler"))
 
 (defmethod whine ((scheduler scheduler) control-string &rest args)
-  (declare (ignore control-string args)))
+  (fire scheduler 'scheduler-log
+        :format-control control-string
+        :format-arguments (copy-list args)))
 
 (defmethod command ((scheduler scheduler) op &rest args)
   (lq:push-queue (cons op args) (command-queue scheduler)))
-
-(defmethod job-thread (job (scheduler scheduler))
-  (gethash job (job-threads scheduler)))
-
-(defmethod (setf job-thread) (new-value job (scheduler scheduler))
-  (setf (gethash job (job-threads scheduler)) new-value))
-
-(defmethod run-job (job (scheduler scheduler))
-  (let ((thread (job-thread job scheduler)))
-    (when (or (null thread)
-              (not (bt:thread-alive-p thread)))
-      (start-job job scheduler))))
-
-(define-condition cancel () ())
-
-(defmethod cancel-job (job (scheduler scheduler))
-  (let ((thread (job-thread job scheduler)))
-    (cond ((null thread))
-          (t
-           (when (bt:thread-alive-p thread)
-             (bt:interrupt-thread thread (lambda () (signal 'cancel))))
-           (setf (job-thread job scheduler) nil)
-           (setf (job-last-run job) (get-universal-time))))))
-
-(defmethod start-job (job (scheduler scheduler))
-  (setf (job-thread job scheduler)
-        (bt:make-thread (lambda () (call-job job scheduler))
-                        :name (format nil "Job ~S" (job-name job)))))
-
-(defmethod call-job (job (scheduler scheduler))
-  (flet ((command (op &rest args)
-           (apply #'command scheduler op args)))
-    (handler-case
-        (handler-bind ((warning
-                        (lambda (condition)
-                          (command :job-warn :job job :condition condition))))
-          (command :job-started :job job)
-          (funcall (job-function job))
-          (command :job-done :job job))
-      (error (condition)
-        (command :job-error :job job :condition condition))
-      (cancel (condition)
-        (command :job-error :job job :condition condition)))))
-
-(defmethod scan-jobs ((scheduler scheduler))
-  (dolist (job (copy-list (jobs scheduler)))
-    (let ((thread (job-thread job scheduler)))
-      (cond ((null thread)
-             (let ((next-run (job-next-run job)))
-               (when (and next-run
-                          (>= (get-universal-time) next-run))
-                 (start-job job scheduler))))
-            ((bt:thread-alive-p thread))
-            (t (whine scheduler "Dead thread for job ~S" (job-name job))
-               (cancel-job job scheduler))))))
 
 (defmethod scheduler-loop ((scheduler scheduler))
   (loop for command = (lq:try-pop-queue (command-queue scheduler)
                                         :timeout 1)
         do (handler-case
-               (if (null command)
-                   (scan-jobs scheduler)
-                   (apply #'process-command scheduler (first command) (rest command)))
+               (bt:with-lock-held ((lock scheduler))
+                 (if (null command)
+                     (scan-jobs scheduler)
+                     (apply #'process-command scheduler (first command) (rest command))))
              (error (condition)
                (whine scheduler "Error in scheduler loop: ~A (command ~S)"
                       condition command))
              (cancel ()
                (whine scheduler "Scheduler is dying")
                (return-from scheduler-loop)))))
+
+(defmethod scan-jobs ((scheduler scheduler))
+  (dolist (job (copy-list (jobs scheduler)))
+    (let ((entry (gethash job (entries scheduler))))
+      (cond ((null (thread entry))
+             (let ((next-run (job-next-run job (start-time entry))))
+               (when (and next-run
+                          (>= (get-universal-time) next-run))
+                 (start-job job entry scheduler))))
+            ((not (bt:thread-alive-p (thread entry)))
+             (whine scheduler "Dead thread for job ~S" job)
+             (cancel-job job entry scheduler))))))
+
+(defmethod start-job (job entry (scheduler scheduler))
+  (setf (status entry) :starting)
+  (setf (thread entry)
+        (bt:make-thread (lambda () (run job))
+                        :name (format nil "Job ~S" (job-name job)))))
+
+(defmethod cancel-job (job entry (scheduler scheduler))
+  (let ((thread (thread entry)))
+    (cond ((null thread))
+          ((bt:thread-alive-p thread)
+           (setf (status entry) :cancelling)
+           (bt:interrupt-thread thread (lambda () (signal 'cancel))))
+          (t
+           (setf (thread entry) nil)))))
+
+(defmethod scheduler-job-event ((scheduler scheduler) job event)
+  (declare (ignore job event)))
+
+(defmethod scheduler-job-event :around ((scheduler scheduler) job event)
+  (bt:with-lock-held ((lock scheduler))
+    (call-next-method)))
+
+(defmethod scheduler-job-event ((scheduler scheduler) job (event job-started))
+  (let ((entry (gethash job (entries scheduler))))
+    (setf (start-time entry) (get-universal-time))
+    (setf (end-time entry) nil)
+    (setf (result entry) nil)
+    (setf (status entry) :running)))
+
+(defmethod scheduler-job-event ((scheduler scheduler) job (event job-error))
+  (let ((entry (gethash job (entries scheduler))))
+    (setf (end-time entry) (get-universal-time))
+    (setf (result entry) (object event))
+    (setf (status entry) :failed)
+    (setf (thread entry) nil)))
+
+(defmethod scheduler-job-event ((scheduler scheduler) job (event job-cancelled))
+  (let ((entry (gethash job (entries scheduler))))
+    (setf (end-time entry) (get-universal-time))
+    (setf (result entry) (object event))
+    (setf (status entry) :cancelled)
+    (setf (thread entry) nil)))
+
+(defmethod scheduler-job-event ((scheduler scheduler) job (event job-done))
+  (let ((entry (gethash job (entries scheduler))))
+    (setf (end-time entry) (get-universal-time))
+    (setf (result entry) nil)
+    (setf (status entry) :done)
+    (setf (thread entry) nil)))
+
+(defmethod scheduler-job-event ((scheduler scheduler) job (event job-warn))
+  (whine scheduler "Job ~S warned: ~A" job (object event)))
 
 (defmacro define-scheduler-command (name (&rest arglist) &body forms)
   `(defmethod process-command ((scheduler scheduler) (op (eql ',name)) &key ,@arglist)
@@ -161,13 +213,28 @@
        ,@forms)))
 
 (define-scheduler-command :add (job)
-  (push job (jobs scheduler)))
+  (let ((listener (lambda (event)
+                    (scheduler-job-event scheduler job event))))
+    (setf (gethash job (entries scheduler))
+          (make-instance 'entry
+                         :start-time nil
+                         :end-time nil
+                         :result nil
+                         :status :pending
+                         :thread nil
+                         :listener listener))
+    (push job (jobs scheduler))
+    (add-listener listener job)))
 
 (define-scheduler-command :remove (job-name)
-  (removef (jobs scheduler) job-name :count 1 :key #'job-name))
+  (let* ((job (find-job job-name))
+         (entry (gethash job (entries scheduler))))
+    (remove-listener (listener entry) job)
+    (removef (jobs scheduler) job :count 1)
+    (remhash job (entries scheduler))))
 
-(define-scheduler-command :list (callback)
-  (funcall callback (jobs scheduler)))
+(define-scheduler-command :request-status ()
+  (fire scheduler 'scheduler-status))
 
 (define-scheduler-command :activate (job-name)
   (setf (job-active-p (find-job job-name)) t))
@@ -176,26 +243,15 @@
   (setf (job-active-p (find-job job-name)) nil))
 
 (define-scheduler-command :run (job-name)
-  (run-job (find-job job-name) scheduler))
+  (let* ((job (find-job job-name))
+         (entry (gethash job (entries scheduler))))
+    (when (null (thread entry))
+      (start-job job entry scheduler))))
 
 (define-scheduler-command :cancel (job-name)
-  (cancel-job (find-job job-name) scheduler))
+  (let* ((job (find-job job-name))
+         (entry (gethash job (entries scheduler))))
+    (cancel-job job entry scheduler)))
 
 (define-scheduler-command :die ()
   (signal 'cancel))
-
-(define-scheduler-command :job-started (job)
-  (whine scheduler "Job ~A started" (job-name job)))
-
-(define-scheduler-command :job-done (job)
-  (cancel-job job scheduler)
-  (whine scheduler "Job ~A completed successfully" (job-name job)))
-
-(define-scheduler-command :job-error (job condition)
-  (cancel-job job scheduler)
-  (whine scheduler "Job ~A failed: ~A" (job-name job) condition))
-
-(define-scheduler-command :job-warn (job condition)
-  (whine scheduler "Job ~A warned: ~A" (job-name job) condition))
-
-(define-scheduler-command :job-progress (job condition))
